1f330463f8b17b0acccce3a0f535b3a30584dc1c
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-04-22 04:34:06 +0700

    Сохраняю картинку

--- encrypt/views.py
+++ encrypt/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-def index(request):
-    return render(request, 'encrypt/homePage.html')
+# def index(request):
+#     return render(request, 'encrypt/homePage.html')
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+from .functions import handle_uploaded_file  
+
+from .forms import StudentForm  
+def index(request):  
+    if request.method == 'POST':  
+        student = StudentForm(request.POST, request.FILES)  
+        if student.is_valid():  
+            handle_uploaded_file(request.FILES['file'])  
+            return HttpResponse("File uploaded successfuly")  
+    else:  
+        student = StudentForm()  
+        return render(request,'encrypt/homePage.html',{'form':student})  
