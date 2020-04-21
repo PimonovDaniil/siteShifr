8a0b649a3a8988152b2cdc60ee180338a524ca72
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-04-22 02:58:30 +0700

    Юзаем html страницу

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h3>Привет мир!</h3>")
+    return render(request, 'mainApp/homePage.html')
