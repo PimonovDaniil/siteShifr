784162ef923c409b800e7c3368b0cbabb2fcf119
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-04-22 21:45:33 +0700

    Revert "роботает вроде"
    This reverts commit f9e5dd122d7bb39b4d505c815e097a22cf987ab2.

--- decrypt/views.py
+++ decrypt/views.py
@@ -12,12 +12,9 @@ def index(request):
     if request.method == 'POST':  
         student = StudentForm(request.POST, request.FILES)  
         if student.is_valid():  
-            try:
-                handle_uploaded_file(request.FILES['file'])  
-                 
-                return render(request,'decrypt/homePage.html',{'form':student,'lol':deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname']))})
-            except:
-                return HttpResponse("Что-то вы ввели не так. Либо это мой косяк.")
+            handle_uploaded_file(request.FILES['file'])  
+            #return HttpResponse(deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname'])))  
+            return render(request,'decrypt/homePage.html',{'form':student,'lol':deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname']))})
     else:  
         student = StudentForm()  
         return render(request,'decrypt/homePage.html',{'form':student})  
