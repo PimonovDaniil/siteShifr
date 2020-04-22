176db3d23732c37d24e1e393a9a4054894974b45
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-04-22 09:12:04 +0700

    Основа сайта готова
    Надо молям всё поправить и не забыть про try в дешифраторе

--- decrypt/views.py
+++ decrypt/views.py
@@ -13,7 +13,8 @@ def index(request):
         student = StudentForm(request.POST, request.FILES)  
         if student.is_valid():  
             handle_uploaded_file(request.FILES['file'])  
-            return HttpResponse(deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname'])))  
+            #return HttpResponse(deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname'])))  
+            return render(request,'decrypt/homePage.html',{'form':student,'lol':deshifr('decrypt/static/upload/'+request.FILES['file'].name,int(request.POST['firstname']))})
     else:  
         student = StudentForm()  
         return render(request,'decrypt/homePage.html',{'form':student})  
