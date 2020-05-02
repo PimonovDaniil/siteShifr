6a6c061b68239318daecbf5554f9a14dc73d2bc9
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-05-02 17:31:37 +0700

    Фиксим БД

--- regis/baza.py
+++ regis/baza.py
@@ -1,8 +1,15 @@
 import json
+import sqlite3
 
 def addUser(put,name,password):
-    with open(put, "r") as read_file:
-        data = json.load(read_file)
+    conn = sqlite3.connect(put)  # или :memory: чтобы сохранить в RAM
+    cursor = conn.cursor()
+    sql = "SELECT * FROM albums WHERE title=?"
+    cursor.execute(sql, [("Red")])
+    for row in cursor.execute("SELECT rowid, * FROM albums ORDER BY title"):
+        data=json.loads(row[1])
+    # with open(put, "r") as read_file:
+    #     data = json.load(read_file)
     res = True
     for k,v in data["users"].items():
         if k == name:
@@ -10,6 +17,17 @@ def addUser(put,name,password):
             break
     if res:
         data["users"][name]=[{},password]
-    with open(put, "w") as write_file:
-        json.dump(data, write_file, indent=4)
+    sql = """
+    UPDATE albums 
+    SET title = '
+    """
+    sql+=json.dumps(data)
+    sql+='\''
+    cursor.execute(sql)
+
+    # Сохраняем изменения
+    conn.commit()
+
+    # with open(put, "w") as write_file:
+    #     json.dump(data, write_file, indent=4)
     return res
